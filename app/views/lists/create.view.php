1f4eaa2c1135ce2eb105b8029022325d911e6e78
laurmertea <mertealaur@yahoo.com>
2023-04-20 11:00:34 +0200

    Update tooltip sign

--- app/views/lists/create.view.php
+++ app/views/lists/create.view.php
@@ -8,7 +8,9 @@ partial('head', compact('title')); ?>
                 <div class="flex-column-center">
                     <div class="flex-center" style="width: -webkit-fill-available;">
                         <div class="tooltip list-item" style="margin-right: 0.5em;">
-                            <div class="dot">i</div>
+                            <div class="dot flex-center">
+                                <span style="font-size: smaller;">i</span>
+                            </div>
                             <span class="tooltipText">Your new list title; required</span>
                         </div>
                         <input type="text" name="title" placeholder="List title" required>
@@ -16,7 +18,9 @@ partial('head', compact('title')); ?>
 
                     <div class="flex-center" style="width: -webkit-fill-available; border-bottom: 1px solid #c3c3c3;">
                         <div class="tooltip list-item" style="margin-right: 0.5em;">
-                            <div class="dot">i</div>
+                            <div class="dot flex-center">
+                                <span style="font-size: smaller;">i</span>
+                            </div>
                             <span class="tooltipText">Your new list description</span>
                         </div>
                         <input type="text" name="description" placeholder="List description">
